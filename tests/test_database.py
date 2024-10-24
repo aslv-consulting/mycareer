79761f6ac433fa6de5972cbf84117aafc27c42d4
Stéphane Lucien-Vauthier <aslv-consulting@outlook.fr>
2024-10-24 17:40:15 +0200

    🔧 chore(ci,#3): Add github workflow to test and lint code before validate a pull request.

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,5 +2,6 @@ from sqlmodel import Session
 from mycareer.database import get_session
 
 def test_get_session() -> None:
+    """Test get_session() function."""
     with get_session() as session:
         assert isinstance(session, Session)
